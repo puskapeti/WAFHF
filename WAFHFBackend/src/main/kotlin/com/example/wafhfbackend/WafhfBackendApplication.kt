a4ab9d3876e4286ce3bff4246a47a42db8fb4e04
Puskapeti <p.puska@lasram.hu>
2022-10-22 11:45:28 +0200

    Initial commit

---
+++ WAFHFBackend/src/main/kotlin/com/example/wafhfbackend/WafhfBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.wafhfbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WafhfBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<WafhfBackendApplication>(*args)
+}
---
+++ WAFHFBackend/src/test/kotlin/com/example/wafhfbackend/WafhfBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wafhfbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WafhfBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
