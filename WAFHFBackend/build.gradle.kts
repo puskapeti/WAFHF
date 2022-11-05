cb2b4f70a4cbbf0d3b56a2e6f61e50b7c4943fa3
puskapeti <puskapeti12@gmail.com>
2022-11-05 14:46:10 +0100

    Configuration updates

--- WAFHFBackend/build.gradle.kts
+++ WAFHFBackend/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.5")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf:2.7.5")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
