f7f6ba5269ecbe9bdd5a923efea144d212f0144c
puskapeti <puskapeti12@gmail.com>
2022-12-05 20:29:22 +0100

    A hosszú leírás hiba megoldva. Mostmár a leírás és a lépések korlátlan hosszúak lehetnek

--- WAFHFBackend/src/main/kotlin/com/example/wafhfbackend/entities/Recipe.kt
+++ WAFHFBackend/src/main/kotlin/com/example/wafhfbackend/entities/Recipe.kt
@@ -7,14 +7,22 @@ class Recipe(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val recipeId: Long? = null,
+
     var name: String,
+
+    @Lob
     var description: String,
+
     @ManyToOne
     @JoinColumn(name = "userId", nullable = false)
     val author: User?,
+
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "recipe_ingredients", joinColumns = [JoinColumn(name = "recipeId")])
     var ingredients: MutableSet<String>,
+
+    @Suppress("JpaAttributeTypeInspection")
+    @Lob
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "recipe_steps", joinColumns = [JoinColumn(name = "recipeId")])
     var steps: MutableSet<String>,
